e80a940cd426507620460e9051636a0deda8f9f2
Alexey Volkov <ark@ark-kun.com>
2014-01-27 06:11:48 +0400

    Rewrote the rewriting code to be SyntaxNode-based, not BoundNode-based. The generated code uses lambdas now. It's inefficient in some instances, but more stable and correct (handles argument conversions and the built-in operators).

--- Ark.Roslyn.Elevator/Elevator.cs
+++ Ark.Roslyn.Elevator/Elevator.cs
@@ -10,25 +10,10 @@ namespace Roslyn.Compilers.CSharp {
             return type.GetMembers().Where(m => m.Name == _opApplicationName).Any(); //TODO: write real checks
         }
 
-        private static BoundExpression CreateDummyTypedExpression(TypeSymbol type) {
-            return new BoundParameter( //replace with something easier
-                default(SyntaxNode),
-                new SourceSimpleParameterSymbol(
-                    default(Symbol),
-                    type,
-                    0,
-                    RefKind.None,
-                    "$_dummy_name_1",
-                    ReadOnlyArray<Location>.Empty
-                ),
-                type
-            );
-        }
-
         internal static BoundExpression BindUnaryOperatorCoreEx(this Binder binder, SyntaxNode syntax, string operatorText, BoundExpression operand, DiagnosticBag diagnostics) {
             DiagnosticBag nonElevatedDiagnostics = DiagnosticBag.GetInstance();
             var boundOperator = binder.BindUnaryOperatorCore(syntax, operatorText, operand, nonElevatedDiagnostics);
-           
+
             if (boundOperator.ResultKind != LookupResultKind.OverloadResolutionFailure) {
                 diagnostics.Add(nonElevatedDiagnostics);
                 nonElevatedDiagnostics.Free();
@@ -57,29 +42,43 @@ namespace Roslyn.Compilers.CSharp {
             DiagnosticBag elevatedDiagnostics = DiagnosticBag.GetInstance();
 
             //Should we unwrap multiple times?
-            TypeSymbol loweredType = elevatedTypeSymbol.TypeArguments.Single();
+            TypeSymbol loweredArgumentType = elevatedTypeSymbol.TypeArguments.Single();
 
-            //TODO:!!!Handle built-in operators
-            //TODO: handle conversions
-            //BoundUnaryOperator loweredOperatorNode = (BoundUnaryOperator)binder.BindUnaryOperatorCore(
-            //    syntax, //change
-            //    operatorText,
-            //    CreateDummyTypedExpression(loweredType),
-            //    elevatedDiagnostics
-            //);
-            BoundUnaryOperator loweredOperatorNode = (BoundUnaryOperator)binder.BindUnaryOperator(
-                Syntax.PrefixUnaryExpression(
-                    syntax.Kind,
-                    Syntax.DefaultExpression(Syntax.ParseTypeName(loweredType.ToDisplayString()))
+            var callSyntax = Syntax.InvocationExpression(
+                Syntax.MemberAccessExpression(
+                    SyntaxKind.MemberAccessExpression,
+                    Syntax.ParseTypeName(elevatedTypeSymbol.ToDisplayString()),
+                    Syntax.IdentifierName(_opApplicationName)
                 ),
-                elevatedDiagnostics
+                Syntax.ArgumentList(
+                    Syntax.SeparatedList<ArgumentSyntax>(
+                        Syntax.Argument((ExpressionSyntax)syntax),
+                        Syntax.Token(SyntaxKind.CommaToken),
+                        Syntax.Argument(
+                            Syntax.ParenthesizedLambdaExpression(
+                                Syntax.ParameterList(
+                                    Syntax.SeparatedList<ParameterSyntax>(
+                                        Syntax.Parameter(Syntax.Identifier("$a"))
+                                            .WithType(Syntax.ParseTypeName(loweredArgumentType.ToDisplayString()))
+                                    )
+                                ),
+                                Syntax.PrefixUnaryExpression(
+                                    syntax.Kind,
+                                    Syntax.IdentifierName("$a")
+                                )
+                            )
+                        )
+                    )
+                )
             );
+            ////var clonedCallSyntax = syntax.SyntaxTree.GetRoot().ReplaceNode((SyntaxNode)syntax, (SyntaxNode)callSyntax);
+            //var clonedCallSyntax = ((SyntaxNode)syntax).ReplaceNode((SyntaxNode)syntax, (SyntaxNode)callSyntax);
+            //var clonedCallSyntax = ((SyntaxNode)syntax.Parent).ReplaceNode((SyntaxNode)syntax, (SyntaxNode)callSyntax);
+            ////var clonedCallSyntax = callSyntax.Green.ToRed();
+            var clonedCallSyntax = SyntaxNode.CloneNodeAsRoot(callSyntax, syntax.SyntaxTree);
 
-            var applicationOperatorCallExpr = binder.BindElevatedOperation(
-                syntax,
-                elevatedTypeSymbol,
-                originalArguments,
-                loweredOperatorNode.MethodOpt,
+            var applicationOperatorCallExpr = binder.BindExpression(
+                clonedCallSyntax,
                 elevatedDiagnostics
             );
 
@@ -126,32 +125,50 @@ namespace Roslyn.Compilers.CSharp {
 
             DiagnosticBag elevatedDiagnostics = DiagnosticBag.GetInstance();
 
-            //Should we unwrap multiple times?
-            TypeSymbol loweredType = elevatedTypeSymbol.TypeArguments.Single();
-
-
             var loweredArgumentTypes = argumentTypes.Select(type => type.IsElevatedType() ? ((NamedTypeSymbol)type).TypeArguments.Single() : type).ToList();
 
-            //Syntax.Parameter(new SyntaxList<AttributeListSyntax>(), new SyntaxTokenList(), Syntax.ParseTypeName(left.Type.ToDisplayString()), Syntax.Identifier("dummy"), null),
-            var loweredOperatorSyntax = Syntax.BinaryExpression(
-                syntax.Kind,
-                Syntax.DefaultExpression(Syntax.ParseTypeName(loweredArgumentTypes[0].ToDisplayString())),
-                Syntax.DefaultExpression(Syntax.ParseTypeName(loweredArgumentTypes[1].ToDisplayString()))
+            var callSyntax = Syntax.InvocationExpression(
+                Syntax.MemberAccessExpression(
+                    SyntaxKind.MemberAccessExpression,
+                    Syntax.ParseTypeName(elevatedTypeSymbol.ToDisplayString()),
+                    Syntax.IdentifierName(_opApplicationName)
+                ),
+                Syntax.ArgumentList(
+                    Syntax.SeparatedList<ArgumentSyntax>(
+                        Syntax.Argument((ExpressionSyntax)left.Syntax),
+                        Syntax.Token(SyntaxKind.CommaToken),
+                        Syntax.Argument((ExpressionSyntax)right.Syntax),
+                        Syntax.Token(SyntaxKind.CommaToken),
+                        Syntax.Argument(
+                            Syntax.ParenthesizedLambdaExpression(
+                                Syntax.ParameterList(
+                                    Syntax.SeparatedList<ParameterSyntax>(
+                                        Syntax.Parameter(Syntax.Identifier("$a"))
+                                            .WithType(Syntax.ParseTypeName(loweredArgumentTypes[0].ToDisplayString())),
+                                        Syntax.Token(SyntaxKind.CommaToken),
+                                        Syntax.Parameter(Syntax.Identifier("$b"))
+                                            .WithType(Syntax.ParseTypeName(loweredArgumentTypes[1].ToDisplayString()))
+                                    )
+                                ),
+                                Syntax.BinaryExpression(
+                                    syntax.Kind,
+                                    Syntax.IdentifierName("$a"),
+                                    syntax.OperatorToken,
+                                    Syntax.IdentifierName("$b")
+                                )
+                            )
+                        )
+                    )
+                )
             );
-            //TODO:!!!Handle built-in operators
-            //TODO: handle conversions
-            BoundExpression loweredOperatorExpr = binder.BindBinaryOperatorCore(
-                loweredOperatorSyntax, //change
-                elevatedDiagnostics
-            );
+            ////var res = syntax.SyntaxTree.GetRoot().ReplaceNode((SyntaxNode)syntax, (SyntaxNode)callSyntax);
+            //var res = ((SyntaxNode)syntax).ReplaceNode((SyntaxNode)syntax, (SyntaxNode)callSyntax);
+            //var res2 = ((SyntaxNode)syntax.Parent).ReplaceNode((SyntaxNode)syntax, (SyntaxNode)callSyntax);
+            ////var redSyntax = callSyntax.Green.ToRed
+            var clonedCallSyntax = SyntaxNode.CloneNodeAsRoot(callSyntax, syntax.SyntaxTree);
 
-            BoundBinaryOperator loweredOperatorNode = (BoundBinaryOperator)loweredOperatorExpr;
-
-            var applicationOperatorCallExpr = binder.BindElevatedOperation(
-                syntax,
-                elevatedTypeSymbol,
-                originalArguments,
-                loweredOperatorNode.MethodOpt,
+            var applicationOperatorCallExpr = binder.BindExpression(
+                clonedCallSyntax,
                 elevatedDiagnostics
             );
 
@@ -159,82 +176,5 @@ namespace Roslyn.Compilers.CSharp {
             elevatedDiagnostics.Free();
             return applicationOperatorCallExpr;
         }
-
-        private static BoundCall BindElevatedOperation(this Binder binder, SyntaxNode originalOperationSyntax, NamedTypeSymbol primaryElevatedTypeSymbol, IEnumerable<BoundExpression> originalArguments, MethodSymbol loweredOperationSymbol, DiagnosticBag diagnostics) {
-            var loweredOperationTargetTypeExpr = new BoundTypeExpression(
-                default(SyntaxNode),
-                (TypeSymbol)loweredOperationSymbol.ContainingSymbol
-            );
-            var elevatedTypeExpr = new BoundTypeExpression(
-                default(SyntaxNode),
-                primaryElevatedTypeSymbol
-            );
-
-            var loweredOperationLookupResult = LookupResult.GetInstance();
-            loweredOperationLookupResult.SetFrom(LookupResult.Good(loweredOperationSymbol));
-            var loweredOperationMethodExpr = new BoundMethodGroup(
-                Syntax.MemberAccessExpression(
-                    SyntaxKind.MemberAccessExpression,
-                    Syntax.IdentifierName("$_dummy_target_2"),
-                    Syntax.IdentifierName("$_dummy_member_2")
-                ),
-                ReadOnlyArray<TypeSymbol>.Empty,
-                loweredOperationTargetTypeExpr,
-                loweredOperationSymbol.Name,
-                ReadOnlyArray.Singleton(loweredOperationSymbol),
-                loweredOperationLookupResult,
-                false
-            );
-            loweredOperationMethodExpr.WasCompilerGenerated = true;
-            loweredOperationLookupResult.Free();
-
-
-            var applicationOperatorCallExpr = binder.BindElevatedOperation(
-                originalOperationSyntax,
-                elevatedTypeExpr,
-                originalArguments,
-                loweredOperationMethodExpr,
-                diagnostics
-            );
-            return applicationOperatorCallExpr;
-        }
-
-        private static BoundCall BindElevatedOperation(this Binder binder, SyntaxNode originalOperationSyntax, BoundTypeExpression primaryElevatedType, IEnumerable<BoundExpression> originalArguments, BoundMethodGroup loweredOperation, DiagnosticBag diagnostics) {
-            var applicationOperatorExpr = binder.BindMemberAccessWithBoundLeft(
-                Syntax.MemberAccessExpression(
-                    SyntaxKind.MemberAccessExpression,
-                    Syntax.IdentifierName("$_dummy_target_3"),
-                    Syntax.IdentifierName("$_dummy_member_3")
-                ),
-                primaryElevatedType,
-                Syntax.IdentifierName(_opApplicationName),
-                Syntax.Token(SyntaxKind.DotToken),
-                true,
-                diagnostics
-            );
-
-            //<BindInvocationExpression - unpacked>
-            AnalyzedArguments arguments = AnalyzedArguments.GetInstance();
-            arguments.Arguments.AddRange(originalArguments);
-            arguments.Arguments.Add(loweredOperation);
-
-            var applicationOperatorCallExpr = binder.BindInvocationExpression(
-                originalOperationSyntax,
-                Syntax.EmptyStatement(),
-                _opApplicationName,
-                applicationOperatorExpr,
-                arguments,
-                diagnostics,
-                null
-            );
-            applicationOperatorCallExpr.WasCompilerGenerated = true;
-            arguments.Free();
-            //</BindInvocationExpression - unpacked>
-
-            return applicationOperatorCallExpr;
-        }
     }
-
-
-
 }
