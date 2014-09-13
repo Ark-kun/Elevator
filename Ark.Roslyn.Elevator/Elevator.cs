84e947155d34c2a28f5497ccc7054265eff64c86
Alexey Volkov <ark@ark-kun.com>
2014-09-13 23:43:18 +0400

    Finally fixed the patched syntax problems by replacing the syntax after getting the BoundNodes.
    This may break for the UnboundLambda part in the future.

--- Ark.Roslyn.Elevator/Elevator.cs
+++ Ark.Roslyn.Elevator/Elevator.cs
@@ -52,40 +52,54 @@ namespace Microsoft.CodeAnalysis.CSharp {
             //Should we unwrap multiple times?
             TypeSymbol loweredArgumentType = elevatedTypeSymbol.TypeArguments.Single();
 
-            var callSyntax = SyntaxFactory.InvocationExpression(
-                SyntaxFactory.MemberAccessExpression(
-                    SyntaxKind.SimpleMemberAccessExpression,
-                    SyntaxFactory.ParseTypeName(elevatedTypeSymbol.ToDisplayString()),
-                    SyntaxFactory.IdentifierName(_opApplicationName)
-                ),
-                SyntaxFactory.ArgumentList(
-                    SyntaxFactory.SeparatedList(
-                        new[] {
-                            SyntaxFactory.Argument((ExpressionSyntax)operand.Syntax),
-                            SyntaxFactory.Argument(
-                                SyntaxFactory.ParenthesizedLambdaExpression(
-                                    SyntaxFactory.ParameterList(
-                                        SyntaxFactory.SingletonSeparatedList(
-                                            SyntaxFactory.Parameter(SyntaxFactory.Identifier("$a"))
-                                                .WithType(SyntaxFactory.ParseTypeName(loweredArgumentType.ToDisplayString()))
-                                        )
-                                    ),
-                                    SyntaxFactory.PrefixUnaryExpression(
-                                        syntax.Kind,
-                                        SyntaxFactory.IdentifierName("$a")
-                                    )
-                                )
-                            )
-                        }
+            var opLambdaSyntax =
+                SyntaxFactory.ParenthesizedLambdaExpression(
+                    SyntaxFactory.ParameterList(
+                        SyntaxFactory.SingletonSeparatedList(
+                            SyntaxFactory.Parameter(SyntaxFactory.Identifier("$a"))
+                                .WithType(SyntaxFactory.ParseTypeName(loweredArgumentType.ToDisplayString()))
+                        )
+                    ),
+                    SyntaxFactory.PrefixUnaryExpression(
+                        syntax.Kind,
+                        SyntaxFactory.IdentifierName("$a")
                     )
-                )
-            );
-            var reintegratedCallSyntax = CSharpSyntaxNode.CloneNodeAsRoot(callSyntax, syntax.SyntaxTree);
+                );
 
-            var applicationOperatorCallExpr = binder.BindExpression(
-                reintegratedCallSyntax,
+            var boundOpLambda = (UnboundLambda)binder.BindExpression(
+                opLambdaSyntax,
                 elevatedDiagnostics
             );
+
+            //The lambda body is not yet bound and we're replacing the syntax. Why doesn't this break everything?
+            //Turns out the new UnboundLambda still links to the old UnboundLambda (with the patch syntax) via the .Data.unboundLambda field.
+            //There is a chance that this (the fact the result of lambda body binding links to the different syntax and tree) may still cause problems.
+            boundOpLambda = boundOpLambda.ReplaceSyntax(syntax);
+
+            var opAccessSyntax = SyntaxFactory.MemberAccessExpression(
+                SyntaxKind.SimpleMemberAccessExpression,
+                SyntaxFactory.ParseTypeName(elevatedTypeSymbol.ToDisplayString()),
+                SyntaxFactory.IdentifierName(_opApplicationName)
+            );
+
+            var boundOpAccess = (BoundMethodGroup)binder.BindExpression(
+                opAccessSyntax,
+                elevatedDiagnostics
+            );
+            boundOpAccess = boundOpAccess.ReplaceSyntax(syntax);
+
+            var callSyntax = SyntaxFactory.InvocationExpression(opAccessSyntax);
+
+            AnalyzedArguments analyzedArguments = AnalyzedArguments.GetInstance();
+            analyzedArguments.Arguments.Add(operand);
+            analyzedArguments.Arguments.Add(boundOpLambda);
+            var name = ((MemberAccessExpressionSyntax)opAccessSyntax).Name.GetUnqualifiedName().Identifier.ValueText;
+            var boundCall = (BoundCall)binder.BindInvocationExpression(callSyntax, callSyntax.Expression, name, boundOpAccess, analyzedArguments, elevatedDiagnostics);
+            analyzedArguments.Free();
+
+            boundCall = boundCall.ReplaceSyntax(syntax);
+
+            var applicationOperatorCallExpr = boundCall;
 
             diagnostics.AddRange(elevatedDiagnostics);
             elevatedDiagnostics.Free();
@@ -130,51 +144,103 @@ namespace Microsoft.CodeAnalysis.CSharp {
 
             var loweredArgumentTypes = argumentTypes.Select(type => type.IsElevatedType() ? ((NamedTypeSymbol)type).TypeArguments.Single() : type).ToList();
 
-            var callSyntax = SyntaxFactory.InvocationExpression(
-                SyntaxFactory.MemberAccessExpression(
-                    SyntaxKind.SimpleMemberAccessExpression,
-                    SyntaxFactory.ParseTypeName(elevatedTypeSymbol.ToDisplayString()),
-                    SyntaxFactory.IdentifierName(_opApplicationName)
-                ),
-                SyntaxFactory.ArgumentList(
-                    SyntaxFactory.SeparatedList(
-                        new[] {
-                            SyntaxFactory.Argument((ExpressionSyntax)left.Syntax),
-                            SyntaxFactory.Argument((ExpressionSyntax)right.Syntax),
-                            SyntaxFactory.Argument(
-                                SyntaxFactory.ParenthesizedLambdaExpression(
-                                    SyntaxFactory.ParameterList(
-                                        SyntaxFactory.SeparatedList(
-                                            new[] {
-                                                SyntaxFactory.Parameter(SyntaxFactory.Identifier("$a"))
-                                                    .WithType(SyntaxFactory.ParseTypeName(loweredArgumentTypes[0].ToDisplayString())),
-                                                SyntaxFactory.Parameter(SyntaxFactory.Identifier("$b"))
-                                                    .WithType(SyntaxFactory.ParseTypeName(loweredArgumentTypes[1].ToDisplayString()))
-                                            }
-                                        )
-                                    ),
-                                    SyntaxFactory.BinaryExpression(
-                                        syntax.Kind,
-                                        SyntaxFactory.IdentifierName("$a"),
-                                        syntax.OperatorToken,
-                                        SyntaxFactory.IdentifierName("$b")
-                                    )
-                                )
-                            )
-                        }
+            var opLambdaSyntax =
+                SyntaxFactory.ParenthesizedLambdaExpression(
+                    SyntaxFactory.ParameterList().AddParameters(
+                        SyntaxFactory.Parameter(SyntaxFactory.Identifier("$arg1"))
+                            .WithType(SyntaxFactory.ParseTypeName(loweredArgumentTypes[0].ToDisplayString())),
+                        SyntaxFactory.Parameter(SyntaxFactory.Identifier("$arg2"))
+                            .WithType(SyntaxFactory.ParseTypeName(loweredArgumentTypes[1].ToDisplayString()))
+                    ),
+                    SyntaxFactory.BinaryExpression(
+                        syntax.Kind,
+                        SyntaxFactory.IdentifierName("$arg1"),
+                        SyntaxFactory.IdentifierName("$arg2")
                     )
-                )
-            );
-            var reintegratedCallSyntax = CSharpSyntaxNode.CloneNodeAsRoot(callSyntax, syntax.SyntaxTree);
+                );
 
-            var applicationOperatorCallExpr = binder.BindExpression(
-                reintegratedCallSyntax,
+            var boundOpLambda = (UnboundLambda)binder.BindExpression(
+                opLambdaSyntax,
                 elevatedDiagnostics
             );
+
+            //The lambda body is not yet bound and we're replacing the syntax. Why doesn't this break everything?
+            //Turns out the new UnboundLambda still links to the old UnboundLambda (with the patch syntax) via the .Data.unboundLambda field.
+            //There is a chance that this (the fact the result of lambda body binding links to the different syntax and tree) may still cause problems.
+            boundOpLambda = boundOpLambda.ReplaceSyntax(syntax);
+
+            var opAccessSyntax = SyntaxFactory.MemberAccessExpression(
+                SyntaxKind.SimpleMemberAccessExpression,
+                SyntaxFactory.ParseTypeName(elevatedTypeSymbol.ToDisplayString()),
+                SyntaxFactory.IdentifierName(_opApplicationName)
+            );
+
+            var boundOpAccess = (BoundMethodGroup)binder.BindExpression(
+                opAccessSyntax,
+                elevatedDiagnostics
+            );
+            boundOpAccess = boundOpAccess.ReplaceSyntax(syntax);
+
+            var callSyntax = SyntaxFactory.InvocationExpression(opAccessSyntax);
+
+            AnalyzedArguments analyzedArguments = AnalyzedArguments.GetInstance();
+            analyzedArguments.Arguments.Add(left);
+            analyzedArguments.Arguments.Add(right);
+            analyzedArguments.Arguments.Add(boundOpLambda);
+            var name = ((MemberAccessExpressionSyntax)opAccessSyntax).Name.GetUnqualifiedName().Identifier.ValueText;
+            var boundCall = (BoundCall)binder.BindInvocationExpression(callSyntax, callSyntax.Expression, name, boundOpAccess, analyzedArguments, elevatedDiagnostics);
+            analyzedArguments.Free();
+
+            boundCall = boundCall.ReplaceSyntax(syntax);
+
+            var applicationOperatorCallExpr = boundCall;
 
             diagnostics.AddRange(elevatedDiagnostics);
             elevatedDiagnostics.Free();
             return applicationOperatorCallExpr;
+        }
+    }
+
+    static class BoundNodeHelpers {
+        public static BoundCall ReplaceSyntax(this BoundCall node, CSharpSyntaxNode syntax) {
+            return new BoundCall(
+                syntax,
+                node.ReceiverOpt,
+                node.Method,
+                node.Arguments,
+                node.ArgumentNamesOpt,
+                node.ArgumentRefKindsOpt,
+                node.IsDelegateCall,
+                node.Expanded,
+                node.InvokedAsExtensionMethod,
+                node.ArgsToParamsOpt,
+                node.ResultKind,
+                node.Type,
+                node.HasErrors
+            );
+        }
+
+        public static UnboundLambda ReplaceSyntax(this UnboundLambda node, CSharpSyntaxNode syntax) {
+            return new UnboundLambda(
+                syntax,
+                node.Data,
+                node.HasErrors
+            );
+        }
+
+        public static BoundMethodGroup ReplaceSyntax(this BoundMethodGroup node, CSharpSyntaxNode syntax) {
+            return new BoundMethodGroup(
+                syntax,
+                node.TypeArgumentsOpt,
+                node.Name,
+                node.Methods,
+                node.LookupSymbolOpt,
+                node.LookupError,
+                node.Flags,
+                node.ReceiverOpt,
+                node.ResultKind,
+                node.HasErrors
+            );
         }
     }
 }
